f6505a230003b420fd2cbdd0689684e329eb26fc
Safwaan Uddin <safwaanuddin@gmail.com>
2023-05-15 16:58:19 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Connection } from '@solana/web3.js';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 
-function App() {
+const connection = new Connection('https://api.mainnet-beta.solana.com');
+const wallet = new PhantomWalletAdapter();
+
+const App = () => {
+  const [connected, setConnected] = useState(false);
+  const [address, setAddress] = useState('');
+
+  const connectWallet = async () => {
+    try {
+      await wallet.connect();
+      setConnected(true);
+      setAddress(wallet.publicKey?.toBase58() || '');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const disconnectWallet = async () => {
+    try {
+      await wallet.disconnect();
+      setConnected(false);
+      setAddress('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {connected ? (
+        <div>
+          <p>Connected Address: {address}</p>
+          <button className='fixed top-0 right-0 p-4' onClick={disconnectWallet}>Disconnect</button>
+        </div>
+      ) : (
+        <button onClick={connectWallet}>Connect</button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
